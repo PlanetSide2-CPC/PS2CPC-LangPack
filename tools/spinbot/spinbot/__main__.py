6fd20220dbd72bdd66eaef867d5bcb76302b628b
Eleven <53446819+voidswordQQJ@users.noreply.github.com>
2022-02-09 20:16:56 -0600

    fix: 修复源文件格式错误

--- tools/spinbot/spinbot/__main__.py
+++ tools/spinbot/spinbot/__main__.py
@@ -5,5 +5,11 @@ from spinbot.convert import CreateConverter
 
 logger = logging.getLogger(__name__)
 
-converter = CreateConverter.get_converter(r"D:\PycharmProjects\PS2CPC-LangPack\tools\spinbot\en_us_data.dat", "xml")
-converter.on_convert()
+path = str(input("源文件路径：") or "../en_us_data.dat")
+types = str(input("转换成（xml/json）：") or "xml")
+
+try:
+    converter = CreateConverter.get_converter(path, types)
+    converter.on_convert()
+except FileNotFoundError:
+    raise FileNotFoundError("你故意找茬是不？你要不要把！")
--- tools/spinbot/spinbot/convert.py
+++ tools/spinbot/spinbot/convert.py
@@ -17,19 +17,38 @@ class Converter(metaclass=abc.ABCMeta):
 
 class XMLConverter(Converter):
     def on_convert(self):
-        with open(self.path, "r") as origin:
-            with open(f"{self.path}.xml", "w") as result:
-                result.writelines("<?xml version='1.0' encoding='UTF-8'?>\n")
-                result.writelines("<root>\n")
-                for line in origin:
-                    components = re.split(r"\s", line, maxsplit=2)
-                    try:
-                        _key, _type, _str = components[0], components[1], components[2].strip()
-                    except IndexError:
-                        continue
-                    _str = _str.replace("<br>", "\n").replace("<br \\>", "\n").replace("<BR>", "\n")
-                    result.writelines(f"  <str key=\"{_key}\" type=\"{_type}\">{_str}</str>\n")
-                result.writelines("</root>")
+        with open(self.path, "r") as origin, open(f"{self.path}.override", "w") as override:
+            contents = ""
+            for line in origin:
+                # 移除语言文件中的空行
+                if line.isspace():
+                    continue
+
+                # 替换 br 标签
+                line = line.replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp;").strip()
+
+                # 重新处理翻译文件空行
+                if any(x in line for x in ["ucdt", "ugdt"]):
+                    contents += f"\n{line}"
+                else:
+                    contents += f"{line}"
+
+            # 保存写入
+            override.writelines(contents.strip())
+
+        with open(f"{self.path}.override", "r") as origin, open(f"{self.path}.xml", "w") as output:
+            # 添加 XML 标头
+            output.writelines("<?xml version='1.0' encoding='UTF-8'?>\n")
+            output.writelines("<root>\n")
+
+            for line in origin:
+                # 分割原始行并转为 XML 格式
+                components = re.split(r"\s", line, maxsplit=2)
+                key, types, string = components[0].strip(), components[1].strip(), components[2].strip()
+                output.writelines(f"  <str key=\"{key}\" type=\"{types}\">{string}</str>\n")
+
+            # 添加 XML 结束标签
+            output.writelines("</root>")
 
 
 class JSONConverter(Converter):
